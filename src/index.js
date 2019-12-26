7cb3fb808e11a34fd6c2648f5a1be63f3d3cfa9e
Ivan160 <dfyzdfyz16@gmail.com>
2019-12-26 23:17:04 +1000

    Fitness on jQuery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "@babel/polyfill";
 import elementClosest from 'element-closest'; 
 elementClosest(window);
+
 import scroll from '../src/modules/scroll';
 import popup from '../src/modules/popup';
 import sendForm from '../src/modules/sendForm';
 import calculator from '../src/modules/calculator';
-
 
 $(function () {
    'use strict';
